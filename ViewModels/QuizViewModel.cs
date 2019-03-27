f16a1d37e4b051c26f734c75f77757c87fba6d8d
Serhii Korol <serhiikorol@MacBook-Pro-Serhii.local>
2019-03-27 16:09:50 +0200

    Created ViewModels folder and created QuizViewModel class

---
+++ ViewModels/QuizViewModel.cs
@@ -0,0 +1,16 @@
+namespace TestMakerFreeWebApp_Chapter_01.ViewModels
+{
+    public class QuizViewModel
+    {
+        public QuizViewModel()
+        {
+            
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Text { get; set; }
+        
+    }
+}
